19d606f0f9617b881df105fd8ad65eadb27ab405
Roman Herrera <46545497+aherreDev@users.noreply.github.com>
2022-09-01 07:42:15 -0500

    Activity 1 (#1)

---
+++ activity_1/App.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hey This is my first React Native App from 7G</Text>
+      <Text>Name: Alberto Roman</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
